40f98d610521849a50f8dce35b98d82f32f1e547
Guillaume Wenzek <gwenzek@users.noreply.github.com>
2026-05-06 21:13:12 +0200

    Revert llvm-disable-loop-vectorization-for-now

--- src/zig_llvm.cpp
+++ src/zig_llvm.cpp
@@ -285,7 +285,7 @@ ZIG_EXTERN_C bool ZigLLVMTargetMachineEmitToFile(LLVMTargetMachineRef targ_machi
     PipelineTuningOptions pipeline_opts;
     pipeline_opts.LoopUnrolling = !options->is_debug;
     pipeline_opts.SLPVectorization = !options->is_debug;
-    pipeline_opts.LoopVectorization = false; // https://github.com/llvm/llvm-project/issues/186922
+    pipeline_opts.LoopVectorization = !options->is_debug;
     pipeline_opts.LoopInterleaving = !options->is_debug;
     pipeline_opts.MergeFunctions = !options->is_debug;
 
